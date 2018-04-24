0ea19ea02101d1dbc07c3a3c58a3e94db86a3e38
katiacortes <katiaocampo@branchbit.com.mx>
2018-04-24 08:30:21 -0500

    24-04-2018

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -1,3 +1,28 @@
 $(function(){
 		$('#iddashboard').addClass('resaltado');
+		
+		Highcharts.chart('container', {
+		    data: {
+		        table: 'datatable'
+		    },
+		    chart: {
+		        type: 'column'
+		    },   
+		    title: {
+		        text: 'Proceso general de tablas'
+		    },
+		    yAxis: {
+		        allowDecimals: false,
+		        title: {
+		            text: 'Units'
+		        }
+		    },
+		    tooltip: {
+		        formatter: function () {
+		            return '<b>' + this.series.name + '</b><br/>' +
+		                this.point.y + ' ' + this.point.name.toLowerCase();
+		        }
+		    }
+		});
 });
+

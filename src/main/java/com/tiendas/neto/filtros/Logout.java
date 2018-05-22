9d08c7b1541161d90f85d672485ff926ba6e4980
abastoGit <agomezm@192.168.2.1>
2018-05-22 14:24:47 -0500

    Mi primer commit :)

--- src/main/java/com/tiendas/neto/filtros/Logout.java
+++ src/main/java/com/tiendas/neto/filtros/Logout.java
@@ -1,11 +1,6 @@
 package com.tiendas.neto.filtros;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;

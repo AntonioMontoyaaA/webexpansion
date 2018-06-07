068210b597848d0c64f25c5ee7ce5b53e64e3f2d
iriverag <iriverag@IvanRivera-PC.neto.corp>
2018-06-07 18:42:47 -0500

    otro

--- src/main/java/com/tiendas/neto/vo/AreasVo.java
+++ src/main/java/com/tiendas/neto/vo/AreasVo.java
@@ -1,5 +1,29 @@
 package com.tiendas.neto.vo;
 
 public class AreasVo {
-
+	String areaId;
+	String area;
+	String puestosRechazo[];
+	
+	public String getAreaId() {
+		return areaId;
+	}
+	public void setAreaId(String areaId) {
+		this.areaId = areaId;
+	}
+	
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	public String[] getPuestosRechazo() {
+		return puestosRechazo;
+	}
+	public void setPuestosRechazo(String[] puestosRechazo) {
+		this.puestosRechazo = puestosRechazo;
+	}
+	
 }
